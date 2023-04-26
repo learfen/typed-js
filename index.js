36917f450af12d9aa23e2340e1736f23b3abf727
unknown <learfen001@gmail.com>
2023-04-26 06:36:44 -0300

    export typed as dec, fix not global

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import typed from "./app/typed/createType.mjs"
-export const create = typed.create 
-export const install = typed.install 
-
-export default { create , install , typed:global.typed }
+import typedCreate from "./app/typed/createType.mjs"
+export const create = typedCreate.create 
+export const install = typedCreate.install 
+export const typed = global.typed
+export default { create , install , typed }
