182c08940fc90557c57e67b131044111ea679b64
Daniel Garcia <learfen001@gmail.com>
2023-07-04 05:32:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import typedCreate from "./app/typed/createType.mjs"
 export const create = typedCreate.create 
 export const install = typedCreate.install 
 export const typed = global.typed
-export default { create , install , typed }
+export const Joi = typedCreate.Joi
+export default { create , install , typed , Joi:typedCreate.Joi}
